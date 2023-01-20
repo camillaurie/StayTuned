817451128972204ad0b49c2cfe0566088d8cb468
camillaurie <camillaurie@gmail.com>
2023-01-20 18:32:09 +0400

    Added turning around in Mattie's hallway feature

--- script/mattiesApartmentIntroduction.js
+++ script/mattiesApartmentIntroduction.js
@@ -10,7 +10,7 @@ function knockingOnMattiesDoor(event) {
         .getElementById("video")
         .innerHTML = `
             <div>
-                <img src="pics/apartmentexterior.png">
+                <img src="pics/ME0N_0.png">
                 <div id="LAAapartmentfrontdoor" style="
                     height: 110px;
                     width: 70px;
@@ -53,7 +53,7 @@ function mattiesHallwayExploration() {
     .getElementById("video")
     .innerHTML = `
         <div>
-            <img src="pics/mattieshallway1.png">
+            <img id="circlingPicture" src="pics/mattie_day_circle/ME1X_0.png">
             <div id="sasa" style="
                 height: 110px;
                 width: 70px;
@@ -62,6 +62,61 @@ function mattiesHallwayExploration() {
                 top: 70px;
                 left: 380px;
             "></div>
+            <div id="turnleftincircle" style="
+                background: transparent;
+                height: 292px;
+                box-shadow: inset 20px 0 20px white;
+                position: absolute;
+                width: 50px;
+                top: 0;
+            "></div>
+            <div id="turnrightincircle" style="
+                background: transparent;
+                height: 292px;
+                box-shadow: inset -20px 0 20px white;
+                width: 50px;
+                position: absolute;
+                top: 0;
+                left: 608px;
+            "></div>
         </div>
     `
+}
+
+document
+    .getElementById("wrapper")
+    .addEventListener("mouseover", Turning); 
+
+function Turning(event) {
+    if (event.target.id == 'turnleftincircle') {
+        if (currentScreenNumber >=18) {
+            currentScreenNumber = -1;
+            increaseScreenNumber();
+        } else {
+            increaseScreenNumber();
+        }
+    }
+    if (event.target.id == 'turnrightincircle') {
+        if (currentScreenNumber <=0) {
+            currentScreenNumber = 17;
+            increaseScreenNumber();
+        } else {
+            decreaseScreenNumber();
+        }
+    }
+}
+
+let currentScreenNumber = 0;
+
+function increaseScreenNumber() {
+    document
+            .getElementById("circlingPicture")
+            .src = "pics/mattie_day_circle/ME1X_" + ++currentScreenNumber + ".png";
+            console.log(currentScreenNumber)
+}
+function decreaseScreenNumber() {
+    document
+            .getElementById("circlingPicture")
+            .src = "pics/mattie_day_circle/ME1X_" + --currentScreenNumber + ".png";
+            console.log(currentScreenNumber)
 }
